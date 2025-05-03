a707d4f5189e7a52bc05ab8a6df3f8a2646c66fb
wale olajumoke <waleolajumoke@gmail.com>
2025-05-03 09:49:24 +0100

    set debug to true

--- movieapp/settings.py
+++ movieapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^*tj!fo#853m8&51u#6d$$i1iwps9s##z_l-0f0gxaaxkdl^bn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['djangomovieapi.onrender.com']
 
